80bd303cce3d48b235c61fb6a605c60f77f2df39
Xiao Cheng <xcheng4@slb.com>
2023-10-29 23:08:18 -0500

    test for authorize middleware

--- webrtc/internal/handler/webrtc_test.go
+++ webrtc/internal/handler/webrtc_test.go
@@ -111,3 +111,126 @@ func TestWebRTCHandler_GetWebRTCConfig(t *testing.T) {
 		})
 	}
 }
+
+type MockHttpHandler struct {
+	mock.Mock
+}
+
+// DoSomething is a method on MyMockedObject that implements some interface
+func (m *MockHttpHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
+	return
+}
+
+func TestWebRTCHandler_Authorize(t *testing.T) {
+	scenarios := []struct {
+		desc                string
+		inRequest           func() *http.Request
+		inWebRTCServiceMock func() *service.MockIWebRTCService
+		inAuthServiceMock   func() *auth.MockIAuthService
+		inHttpHandlerMock   func() http.Handler
+		expectedStatus      int
+		expectedPayload     string
+	}{
+		{
+			desc: "Happy Path",
+			inRequest: func() *http.Request {
+				// perform request
+				requestData := &dto.GetWebRTCConfigRequest{}
+				data, _ := json.Marshal(requestData)
+				body := bytes.NewBuffer(data)
+				request, err := http.NewRequest("GET", "/", body)
+				// define context and therefore test timeout
+				ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+				defer cancel()
+				request = request.WithContext(ctx)
+				request.Header.Set("Content-Type", "application/json")
+				require.NoError(t, err)
+				return request
+			},
+			inWebRTCServiceMock: func() *service.MockIWebRTCService {
+				output := &domain.RTCConfig{
+					LifetimeDuration:   "3600s",
+					BlockStatus:        "NOT_BLOCKED",
+					IceTransportPolicy: "all",
+					IceServers:         []domain.ICEServer{},
+				}
+				mockWebRTCService := &service.MockIWebRTCService{}
+				mockWebRTCService.On("GetWebRTCConfig", mock.Anything, mock.Anything).Return(output, nil).Once()
+				return mockWebRTCService
+			},
+			inAuthServiceMock: func() *auth.MockIAuthService {
+				mockAuthService := &auth.MockIAuthService{}
+				mockAuthService.On("Authorize", mock.Anything).Return("user", nil).Once()
+				return mockAuthService
+			},
+			inHttpHandlerMock: func() http.Handler {
+				mockHttpHandler := new(MockHttpHandler)
+				mockHttpHandler.On("ServeHTTP", mock.Anything, mock.Anything).Return()
+				return mockHttpHandler
+			},
+			expectedStatus:  http.StatusOK,
+			expectedPayload: "",
+		},
+		{
+			desc: "401: Unauthorized",
+			inRequest: func() *http.Request {
+				// perform request
+				requestData := &dto.GetWebRTCConfigRequest{}
+				data, _ := json.Marshal(requestData)
+				body := bytes.NewBuffer(data)
+				request, err := http.NewRequest("GET", "/", body)
+				// define context and therefore test timeout
+				ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+				defer cancel()
+				request = request.WithContext(ctx)
+				request.Header.Set("Content-Type", "application/json")
+				require.NoError(t, err)
+				return request
+			},
+			inWebRTCServiceMock: func() *service.MockIWebRTCService {
+				output := &domain.RTCConfig{
+					LifetimeDuration:   "3600s",
+					BlockStatus:        "NOT_BLOCKED",
+					IceTransportPolicy: "all",
+					IceServers:         []domain.ICEServer{},
+				}
+				mockWebRTCService := &service.MockIWebRTCService{}
+				mockWebRTCService.On("GetWebRTCConfig", mock.Anything, mock.Anything).Return(output, nil).Once()
+				return mockWebRTCService
+			},
+			inAuthServiceMock: func() *auth.MockIAuthService {
+				mockAuthService := &auth.MockIAuthService{}
+				mockAuthService.On("Authorize", mock.Anything).Return("", auth.NewUnauthorizedError("empty bearerToken")).Once()
+				return mockAuthService
+			},
+			inHttpHandlerMock: func() http.Handler {
+				mockHttpHandler := new(MockHttpHandler)
+				mockHttpHandler.On("ServeHTTP", mock.Anything, mock.Anything).Return()
+				return mockHttpHandler
+			},
+			expectedStatus:  http.StatusUnauthorized,
+			expectedPayload: "{\"status\":\"Not Authorized\",\"error\":\"empty bearerToken\"}\n",
+		},
+	}
+	for _, s := range scenarios {
+		scenario := s
+		t.Run(scenario.desc, func(t *testing.T) {
+			mockWebRTCService := scenario.inWebRTCServiceMock()
+			mockAuthService := scenario.inAuthServiceMock()
+			mockHttpHandler := scenario.inHttpHandlerMock()
+			// build handler
+			webrtcHandler := NewWebRTCHandler(mockWebRTCService, mockAuthService)
+
+			// perform request
+			response := httptest.NewRecorder()
+			handler := webrtcHandler.Authorize(mockHttpHandler)
+			handler.ServeHTTP(response, scenario.inRequest())
+
+			// validate outputs
+			require.Equal(t, scenario.expectedStatus, response.Code, scenario.desc)
+
+			payload, _ := io.ReadAll(response.Body)
+			assert.Equal(t, scenario.expectedPayload, string(payload), scenario.desc)
+		})
+	}
+}
